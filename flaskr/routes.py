e57cd71ae67faf5286ff8dfb0c67f519ecdfd3e7
Jingbo Han <danabaxia@gmail.com>
2021-03-17 17:47:58 -0500

    update login, error

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -2,17 +2,30 @@ from flask import render_template, flash, redirect
 from flask.helpers import url_for
 from flask_login.utils import login_required
 from flaskr import app
-from flaskr.forms import LoginForm
+from flaskr.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user
 from flaskr.models import User
 from flask_login import logout_user
 from flask import request
 from werkzeug.urls import url_parse
+from flaskr import db
+from datetime import datetime
+from flaskr.forms import EditProfileForm
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
     return render_template('index.html', title='Home Page', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -36,3 +49,51 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+#record user last visit 
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+ 
+#edit user profile
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
