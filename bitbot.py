6170eb237aedf0eaedfe0b70754240019d8bdae3
danabaxia <danabaxia@gmail.com>
2021-04-09 16:40:13 -0500

    update activities page design

--- bitbot.py
+++ bitbot.py
@@ -1,6 +1,6 @@
 from flaskr import app, db
-from flaskr.models import User
+from flaskr.models import User, Transaction
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User}
+    return {'db': db, 'User': User, 'Transaction': Transaction}
--- flaskr/models.py
+++ flaskr/models.py
@@ -9,6 +9,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    phone = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     #posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
@@ -27,6 +28,16 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    action = db.column(db.String(16))
+    order = db.column(db.String(16))
+    status = db.column(db.String(16))
+    amount = db.column(db.Float)
+    price = db.column(db.Float)
+    user_id = db.column(db.Integer, db.ForeignKey('user.id'))
 
 """class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
