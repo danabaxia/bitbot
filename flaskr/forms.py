71170132fce0f316b623556515358495ad5ca131
jzhu118 <58021801+jzhu118@users.noreply.github.com>
2021-04-08 22:25:08 -0700

    Added more models
    Added more functions for User and also created 3 other models

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from flaskr.models import User
+# from flask.ext.mysql import MySQL
+# from flask import Flask, render_template, request
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
--- flaskr/models.py
+++ flaskr/models.py
@@ -6,38 +6,79 @@ from flaskr import login
 from hashlib import md5
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'Users'
+
+    user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    #posts = db.relationship('Post', backref='author', lazy='dynamic')
-    about_me = db.Column(db.String(140))
-    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    mobilenumber = db.Column(db.String(128))
+    address = db.Column(db.String(128))
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)    
+    @property
+    def password(self):
+        """
+        Prevent pasword from being accessed
+        """
+        raise AttributeError('password is not a readable attribute.')
 
-    def set_password(self, password):
+    @password.setter
+    def password(self, password):
+        """
+        Set password to a hashed password
+        """
         self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
+    def verify_password(self, password):
+        """
+        Check if hashed password matches actual password
+        """
         return check_password_hash(self.password_hash, password)
 
-    def avatar(self, size):
-        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
-            digest, size)
+    def __repr__(self):
+        return '<Employee: {}>'.format(self.username
+        
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
-"""class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class Transaction(UserMixin, db.Model):
+    __tablename__ = 'Transactions'
+
+    transaction_id = db.Column(db.Integer, primary_key=True)
+    transaction_type = db.Column(db.String(64), index=True)
+    transaction_bitcoin_number = db.Column(db.Integer, index=True)
+    bitcoin_date = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer)
 
     def __repr__(self):
-        return '<Post {}>'.format(self.body)"""
+        return '<Transaction: {}>'.format(self.transaction_id)
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+
+class Product(UserMixin, db.Model):
+    __tablename__ = 'Products'
+
+    product_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    subcription_type = db.Column(db.String(64), index=True)
+
+    def __repr__(self):
+        return '<Product: {}>'.format(self.product_id)
+
+
+class Strategy(UserMixin, db.Model):
+    __tablename__ = 'Strategies'
+
+    product_id = db.Column(db.Integer, primary_key=True)
+    strategy_name = db.Column(db.String(64), index=True)
+    product_strategy_algorithm = db.Column(db.String(64), index=True)
+
+    def __repr__(self):
+        return '<Strategy: {}>'.format(self.product_id)
+
+
+
+
+
 
